9d3936832ea08e894653184195463bc3b96556b8
giulianaienco1@gmail.com <giulianaienco1@gmail.com>
2023-09-08 16:04:17 -0300

    Hicimos el fetching y agregamos rutas

--- Komiditas Rikas/src/App.jsx
+++ Komiditas Rikas/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { ContextProvider } from './context/UseContext';
+import { ContextProvider } from './UseContext';
 import './App.css'
+import Menu from "./components/Menu.jsx";
 
 function App() {
   return (
     <ContextProvider>
-      
+      <Menu/>
     </ContextProvider>
   );
 }
