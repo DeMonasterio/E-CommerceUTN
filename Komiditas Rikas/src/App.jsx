001b1078098a53c8141c5f4bef39a0264fa1a58c
de Monasterio <juanmanueldemonasterio@gmail.com>
2023-09-08 15:10:06 -0300

    Inicializando repositorio

---
+++ Komiditas Rikas/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ContextProvider } from './context/UseContext';
+import './App.css'
+
+function App() {
+  return (
+    <ContextProvider>
+      
+    </ContextProvider>
+  );
+}
+
+export default App;
