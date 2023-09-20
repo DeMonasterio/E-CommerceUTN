1cebe2051996c74f69ce39edaa2c730a69f06d05
giulianaienco1@gmail.com <giulianaienco1@gmail.com>
2023-09-20 20:02:20 -0300

    Lógica de los filtros modularizada en custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -4,90 +4,37 @@ import ProductCard from "./components/ProductCard";
 import NavBar from "./components/NavBar.jsx";
 import SearchBar from "./components/SearchBar.jsx";
 import Filters from "./components/Filters.jsx";
-
-
-
-
-
-
-
+import useProductData from "./hooks/useProductData";
+import useProductSorting from "./hooks/useProductSorting";
 
 function App() {
-  const [minToMaxPrice, setminToMaxPrice] = useState(0);
-  const [maxToMinPrice, setmaxtoMinPrice] = useState(0);
-  const [Alphab, setAlphab] = useState()
-  const [products, setProducts] = useState([]);
+  const products = useProductData();
+  const [minToMaxPrice, setMinToMaxPrice] = useState(false);
+  const [maxToMinPrice, setMaxToMinPrice] = useState(false);
+  const [Alphab, setAlphab] = useState(false);
   const [searchedText, setSearchedText] = useState("");
   const [RangeValue, setRangeValue] = useState(1000);
-  const arrayProducts = [];
 
-
-
-
-
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((prod) => setProducts(prod));
-  }, []);
-
-  //  Ordenamiento de mayor a menor
-  const sortProducts = (product, arrayProducts) => {
-    arrayProducts.push(product);
-    arrayProducts.sort((a, b) => a.price - b.price);
-  }
-
-  const sortProductsMax = (product, arrayProducts) => {
-    arrayProducts.push(product);
-    arrayProducts.sort((a, b) => b.price - a.price);
-  }
-
-  const noOrderCase = (product, arrayProducts) => {
-    arrayProducts.push(product);
-  }
-  if (maxToMinPrice || minToMaxPrice === true) {
-    if (minToMaxPrice === true && maxToMinPrice === false) {
-      for (const product of products) {
-        sortProducts(product, arrayProducts)
-      }
-    }
-    else if (maxToMinPrice === true && minToMaxPrice == false) {
-      for (const product of products) {
-        sortProductsMax(product, arrayProducts)
-      }
-    }
-    //  Fin del Ordenamiento de mayor a menor
-  } else {
-    for (const product of products) {
-      noOrderCase(product, arrayProducts)
-    }
-  }
-  if (Alphab === true) {
-    const rta = arrayProducts.sort(function (a, b) {
-      if (a.title.toLowerCase() < b.title.toLowerCase()) { return -1; }
-      if (a.title.toLowerCase() > b.title.toLowerCase()) { return 1; }
-      return 0;
-    })
-  }
-
-
-
-
-
+  const sortedProducts = useProductSorting(products, minToMaxPrice, maxToMinPrice, Alphab);
 
   return (
     <>
-        <NavBar />
+      <NavBar />
       <header className="hero__fix">
         <SearchBar setSearchedText={setSearchedText} />
-        <Filters RangeValue={RangeValue} setRangeValue={setRangeValue}
-          setmaxtoMinPrice={setmaxtoMinPrice}
-          setminToMaxPrice={setminToMaxPrice} setAlphab={setAlphab} />
+        <Filters
+          RangeValue={RangeValue}
+          setRangeValue={setRangeValue}
+          setMinToMaxPrice={setMinToMaxPrice} 
+          setMaxToMinPrice={setMaxToMinPrice} 
+          setAlphab={setAlphab}
+        />
       </header>
       <main>
-
         <div className="products--container">
-          {arrayProducts.map((p) => (<ProductCard p={p} key={p.id} searchedText={searchedText} RangeValue={RangeValue} />))}
+          {sortedProducts.map((p) => (
+            <ProductCard p={p} key={p.id} searchedText={searchedText} RangeValue={RangeValue} />
+          ))}
         </div>
       </main>
     </>
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,79 +1,89 @@
-import React, { useRef } from 'react'
-import "./css/Filters.css"
-export default function Filters({ RangeValue, setRangeValue, setminToMaxPrice, setmaxtoMinPrice, setAlphab }) {
+import React, { useRef, useEffect } from 'react';
+import "./css/Filters.css";
 
-    const Bettini = useRef(null);
-    const Marce = useRef(null);
-    const voTaLocoVite = useRef(null); 
+export default function Filters({
+  RangeValue,
+  setRangeValue,
+  setMinToMaxPrice,
+  setMaxToMinPrice,
+  setAlphab,
+  minToMaxPrice,
+  maxToMinPrice,
+  Alphab,
+}) {
+  const Bettini = useRef(null);
+  const Marce = useRef(null);
+  const voTaLocoVite = useRef(null);
 
-    const handleChange = (e) => {
-        setRangeValue(e.target.value);
+  useEffect(() => {
+    if (Bettini.current) {
+      Bettini.current.checked = minToMaxPrice;
     }
-
-    const handleCheck = (e) => {
-        if (Bettini.current && Marce.current && voTaLocoVite.current) {
-            if (e.target === Bettini.current) {
-                Marce.current.checked = false
-                voTaLocoVite.current.checked = false
-                setmaxtoMinPrice(false)
-                setAlphab(false)
-                setminToMaxPrice(Bettini.current.checked)
-                
-            }
-            if (e.target === Marce.current) {
-                Bettini.current.checked = false
-                voTaLocoVite.current.checked = false
-                setminToMaxPrice(false)
-                setAlphab(false)
-                setmaxtoMinPrice(Marce.current.checked)
-            }
-            if (e.target === voTaLocoVite.current){
-                Bettini.current.checked = false
-                Marce.current.checked = false
-                setmaxtoMinPrice(false)
-                setminToMaxPrice(false)
-                setAlphab(voTaLocoVite.current.checked)
-            }
-        }
+    if (Marce.current) {
+      Marce.current.checked = maxToMinPrice;
     }
+    if (voTaLocoVite.current) {
+      voTaLocoVite.current.checked = Alphab;
+    }
+  }, [minToMaxPrice, maxToMinPrice, Alphab]);
 
+  const handleChange = (e) => {
+    setRangeValue(e.target.value);
+  }
 
+  const handleCheck = (e) => {
+    if (Bettini.current && Marce.current && voTaLocoVite.current) {
+      if (e.target === Bettini.current) {
+        Marce.current.checked = false;
+        voTaLocoVite.current.checked = false;
+        setMaxToMinPrice(false);
+        setAlphab(false);
+        setMinToMaxPrice(Bettini.current.checked);
+      }
+      if (e.target === Marce.current) {
+        Bettini.current.checked = false;
+        voTaLocoVite.current.checked = false;
+        setMinToMaxPrice(false);
+        setAlphab(false);
+        setMaxToMinPrice(Marce.current.checked);
+      }
+      if (e.target === voTaLocoVite.current) {
+        Bettini.current.checked = false;
+        Marce.current.checked = false;
+        setMaxToMinPrice(false);
+        setMinToMaxPrice(false);
+        setAlphab(voTaLocoVite.current.checked);
+      }
+    }
+  }
 
-    return (
-        <div className='filter__container'>
-
-
-            <form className='filter__range'>
-                <label htmlFor="rango">Precio Máximo</label>
-                <input className='inputcolor' onChange={handleChange} value={RangeValue} type="range" id="rango" name="rango" min="0" max="1000" step="1" />
-                <output htmlFor="rango" id="valor">{RangeValue}</output>
-            </form>
-
-
-            <input className='filter__checkbox' type="checkbox" name="" id="filtrado" />
-            <label className='filter__label' htmlFor="filtrado">
-                Filtrar
-                <span className='filter__icon'></span>
-                <span className='filter__icon'></span>
-            </label>
-
-
-            <div className='filterscontainer'>
-                <div className='div'>
-                    <label htmlFor="minPrice">Menor a Mayor</label>
-                    <input type="checkbox" id='minprice' ref={Bettini} onChange={e => handleCheck(e)} />
-                </div>
-
-                <div className='div'>
-                    <label htmlFor="maxprice">Mayor a Menor</label>
-                    <input type="checkbox" id='maxprice' ref={Marce} onChange={e => handleCheck(e)} />
-                </div>
-
-                <div className='div'>
-                    <label htmlFor="alphab">Alfabeticamente</label>
-                    <input type="checkbox" id='alphab' ref={voTaLocoVite} onChange={(e) => handleCheck(e)} />
-                </div>
-            </div>
+  return (
+    <div className='filter__container'>
+      <form className='filter__range'>
+        <label htmlFor="rango">Precio Máximo</label>
+        <input className='inputcolor' onChange={handleChange} value={RangeValue} type="range" id="rango" name="rango" min="0" max="1000" step="1" />
+        <output htmlFor="rango" id="valor">{RangeValue}</output>
+      </form>
+      <input className='filter__checkbox' type="checkbox" name="" id="filtrado" />
+      <label className='filter__label' htmlFor="filtrado">
+        Filtrar
+        <span className='filter__icon'></span>
+        <span className='filter__icon'></span>
+      </label>
+      <div className='filterscontainer'>
+        <div className='div'>
+          <label htmlFor="minPrice">Menor a Mayor</label>
+          <input type="checkbox" id='minprice' ref={Bettini} onChange={e => handleCheck(e)} />
         </div>
-    )
+        <div className='div'>
+          <label htmlFor="maxprice">Mayor a Menor</label>
+          <input type="checkbox" id='maxprice' ref={Marce} onChange={e => handleCheck(e)} />
+        </div>
+        <div className='div'>
+          <label htmlFor="alphab">Alfabeticamente</label>
+          <input type="checkbox" id='alphab' ref={voTaLocoVite} onChange={(e) => handleCheck(e)} />
+        </div>
+      </div>
+    </div>
+  );
 }
